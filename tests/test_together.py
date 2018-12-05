86348b988bc7e51657ed2f10d62fb1b9052868e3
DustyShap <dustinshapiro@gmail.com>
2018-12-05 08:23:02 -0600

    Add tests

--- tests/test_together.py
+++ tests/test_together.py
@@ -1,12 +1,32 @@
 import os
 import sys
+import csv
 import unittest
 import pytest
+import mock
+# import unittest.mock
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 
 from together.together import Together
 
-@pytest.fixture
-def together_name():
-    return Together()
+class TogetherTest(unittest.TestCase):
+
+    def setUp(self):
+        self.together = Together('test_list.csv')
+
+    def test_it_loads_input_file(self):
+        with open(self.together.input_file) as f:
+            t = csv.reader(f)
+        assert '_csv.reader' in str(t)
+
+    def test_it_loads_file_into_list(self):
+        self.together.csv_read_to_list()
+        assert type(self.together.name_list) == list
+        assert len(self.together.name_list) == 5
+
+    def test_it_chooses_group_size(self):
+        with mock.patch('builtins.input', return_value='6'):
+            self.together.set_group_size()
+            assert self.together.group_size == 6
+            assert type(self.together.group_size) == int
--- together/together.py
+++ together/together.py
@@ -2,16 +2,44 @@ import csv
 import sys
 
 class Together:
+
     def __init__(self, input_file):
         self.input_file = input_file
-        self.output_file = None
-        self.name_list = None
-
+        self.name_list = []
+        self.output_list = []
+        self.group_size = None
 
     def load_input_file(self):
         """Load input CSV File"""
-        pass
+        try:
+            open(self.input_file)
+        except FileNotFoundError:
+            print('No file found for {}'.format(self.input_file))
+            print('Usage: scripts/generate <input_file.csv>')
+            sys.exit()
+
+    def csv_read_to_list(self):
+        with open(self.input_file) as f:
+            reader = csv.reader(f)
+            self.name_list = [n[0].strip() for n in list(reader)]
+
+    def set_group_size(self):
+        valid_input = False
+        while not valid_input:
+            r = input('Group Size: ')
+            if r.isdigit() and r != '0':
+                self.group_size = int(r)
+                valid_input = True
+            else:
+                print('Please enter a valid number')
+
+
+
+
+
 
 
     def run(self):
         self.load_input_file()
+        self.csv_read_to_list()
+        self.set_group_size()
